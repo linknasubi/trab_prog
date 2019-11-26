856a1d249e30fcf24a7466d3f8a4d4eda47f0bd5
Gabriel Oliveira Aragão <31046857+linknasubi@users.noreply.github.com>
2019-11-26 02:41:14 -0300

    Algoritmo funcional
    Algoritmo buscando os elementos requisitados e criando a planilha no excel com os tamanhos de acordo.

--- teste_marcelle.py
+++ teste_marcelle.py
@@ -1,4 +1,7 @@
 import xml.dom.minidom as mnd
+import pandas as pd
+from pandas import ExcelWriter
+
 
 doc = mnd.parse("curriculo.xml")
 teste = doc.getElementsByTagName("TRABALHO-EM-EVENTOS")
@@ -9,6 +12,8 @@ ano = []
 tit_cnf = []
 doi = []
 dic = {}
+contador = 0
+
 
 #print(teste)
 for i in teste:
@@ -27,8 +32,19 @@ for i in childs2:
     tit_cnf.append(i[0].attributes['TITULO-DOS-ANAIS-OU-PROCEEDINGS'].value)
     
 for i,j,k,l in zip (tit,ano,tit_cnf,doi):
+    contador += 1
+    dic[str(j)] = {"Title":str(i), "Conference Title":str(k), "DOI": str(l)}
     
-    dic[str(i)] =  ("Year:"+str(j),"Conference Title:"+str(k),"Doi:"+str(l))
-           
-#{str(i) +":", ("Year:"+str(j),"Conference Title:"+str(k),"Doi:"+str(l))}
-print(dic)
+
+df = pd.DataFrame(dic)
+writer = ExcelWriter("Teste_10.xlsx")
+workbook=writer.book
+formato = workbook.add_format({'text_wrap': True})
+df.to_excel(writer, "Sheet1", index=True)
+worksheet = writer.sheets['Sheet1']
+worksheet.set_column(0, contador, 20, formato)
+for i in range(contador):
+    worksheet.set_row(i,90)
+writer.save()
+writer.close()
+
